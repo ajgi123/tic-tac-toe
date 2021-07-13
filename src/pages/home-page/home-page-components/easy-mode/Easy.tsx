82e1bfd85c4a4f5842ea5806b0dbd48890f93e25
ajgi123 <igorszukalo@gmail.com>
2021-07-13 20:29:54 +0200

    Refactor props added misiing dependacies

--- src/pages/home-page/home-page-components/easy-mode/Easy.tsx
+++ src/pages/home-page/home-page-components/easy-mode/Easy.tsx
@@ -1,82 +1,63 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { generateEmptyGameState } from "../../../../helpers/generateEmptyGameState";
 import Button from "../../../../components/atoms/button/Button";
-import useLocalStorage from "../../../../hooks/useLocalStorage";
 import { randomAi } from "../../../../logic/AI";
+import useUndoRedo from "../../../../hooks/useUndoRedo";
 import { CellKind } from "../../../../types/cellKind";
 
 type EasyPropsType = {
   gameState: CellKind[];
   turn: CellKind;
-  moveAI: (index: number) => void;
   changeState: (gameState: CellKind[]) => void;
   isWin: boolean;
 };
 
-const initialState = {
-  gameStates: [generateEmptyGameState(3)],
-  index: 0,
-};
+const initialState = [generateEmptyGameState(3)];
 
-const Easy = (props: EasyPropsType) => {
-  const [history, setHistory] = useLocalStorage("history", initialState);
+const Easy = ({ gameState, turn, changeState, isWin }: EasyPropsType) => {
+  const { history, undo, redo, resetHistory, addItemToHistory } = useUndoRedo(
+    "history",
+    initialState
+  );
+  const initialRender = useRef(true);
 
   useEffect(() => {
-    if (props.isWin) {
-      setHistory(initialState);
+    if (initialRender.current) {
+      initialRender.current = false;
+    }
+
+    changeState(history.array[history.index]);
+  }, [history.index, history.array, changeState]);
+
+  useEffect(() => {
+    if (isWin) {
+      resetHistory();
       return;
     }
 
-    if (props.turn === CellKind.Cross) {
+    if (turn === CellKind.Cross) {
       const timeout = setTimeout(() => {
-        const gameStateCopy = [...props.gameState];
-        const indexMove = randomAi(props.gameState);
+        const gameStateCopy = [...gameState];
+        const indexMove = randomAi(gameState);
         gameStateCopy[indexMove] = CellKind.Cross;
-        setHistory((prevState) => {
-          let gameStates = [...prevState.gameStates];
-          let index = prevState.index;
-          if (index < gameStates.length - 1) {
-            gameStates = gameStates.slice(0, prevState.index + 1);
-            index = gameStates.length - 1;
-          }
-          index++;
-          return {
-            gameStates: [...gameStates, [...gameStateCopy]],
-            index: index,
-          };
-        });
-        props.moveAI(indexMove);
+        addItemToHistory(gameStateCopy);
       }, 300);
       return () => {
         clearTimeout(timeout);
       };
     }
-  }, [props.turn, props.isWin]);
-
-  const undo = () => {
-    props.changeState(history.gameStates[history.index - 1]);
-    setHistory((prevState) => {
-      return { ...prevState, index: prevState.index - 1 };
-    });
-  };
-
-  const redo = () => {
-    props.changeState(history.gameStates[history.index + 1]);
-    setHistory((prevState) => {
-      return { ...prevState, index: prevState.index + 1 };
-    });
-  };
+  }, [turn, isWin, gameState, resetHistory, addItemToHistory]);
 
   return (
     <div>
       <Button
         onClick={redo}
-        disabled={history.index >= history.gameStates.length - 1 || props.isWin}
+        disabled={history.index >= history.array.length - 1 || isWin}
         name="REDO"
       />
       <Button
         onClick={undo}
-        disabled={history.index <= 0 || props.isWin}
+        disabled={history.index <= 0 || isWin}
         name="UNDO"
       />
     </div>
