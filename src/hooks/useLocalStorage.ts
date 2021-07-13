3cc90b5edf5fda7496735798ed8324fa031919e6
ajgi123 <igorszukalo@gmail.com>
2021-07-13 20:17:43 +0200

    Add missing dependacies added usecallback

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,20 +1,26 @@
 import { localStorageService } from "../helpers/localStorageService";
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import {
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useEffect,
+  useState,
+} from "react";
 
 const useLocalStorage = <T>(
   key: string,
   initialValue: T
 ): [T, Dispatch<SetStateAction<T>>] => {
-  const initialiazeState = () => {
+  const initialiazeState = useCallback(() => {
     const localData = localStorageService.getItem<T>(key, initialValue);
     return localData;
-  };
+  }, [key, initialValue]);
 
   const [storedValue, setStoredValue] = useState<T>(initialiazeState);
 
   useEffect(() => {
     setStoredValue(initialiazeState);
-  }, [key]);
+  }, [initialiazeState]);
 
   useEffect(() => {
     localStorageService.setItem(key, storedValue);
